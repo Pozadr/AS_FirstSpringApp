e1732fea742242fe5c26c82870c880ebc758a35f
Pozadr <adpozor@gmail.com>
2020-10-28 11:09:20 +0100

    Profiles

--- src/main/java/pl/pozadr/springfirstapp/SpringFirstAppApplication.java
+++ src/main/java/pl/pozadr/springfirstapp/SpringFirstAppApplication.java
@@ -2,9 +2,7 @@ package pl.pozadr.springfirstapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
@@ -13,11 +11,6 @@ public class SpringFirstAppApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringFirstAppApplication.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void helloWorld() {
-        System.out.println("Hello World!");
     }
 
     @Bean
